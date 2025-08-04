5ca82ca8751d666797ae8fe9e85f685cc0acff25
aaaa <2220140@ecc.ac.jp>
2025-08-04 16:19:44 +0900

    0804ルーティーン詳細画面の開催ボタンの作成

--- lib/view/page/routine_details.dart
+++ lib/view/page/routine_details.dart
@@ -61,6 +61,8 @@ class _RoutineDetailState extends State<RoutineDetail> {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
+    //仮で用意
+    final mochi_id = "miumiu";
 
     ///API取得失敗やnullの場合の表示
     if (routine == null) {
@@ -171,7 +173,11 @@ class _RoutineDetailState extends State<RoutineDetail> {
           ),
         ),
       ),
-      floatingActionButton: _joinButton(),
+
+      //仮で用意した変数mochi_idが、ルーティーン作成者と一致すれば開催ボタンに、一致しなければ参加ボタンに
+      floatingActionButton: mochi_id == routine!.user.mochiId 
+      ? _holdButton() 
+      : _joinButton(),
     );
   }
 
@@ -216,4 +222,120 @@ class _RoutineDetailState extends State<RoutineDetail> {
       ),
     );
   }
+
+  //開催ボタン
+  Widget _holdButton() {
+    return ElevatedButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            //ダイアログ
+            return AlertDialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+                side: BorderSide(color: Colors.red, width: 1),
+              ),
+              title: Container(
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                  icon: Icon(Icons.close, color: Colors.red),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+                ),
+              ),
+              content: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Stack(
+                  children: [
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        //仮で時間を表示してます
+                        // 所要時間
+                        Row(
+                          children: [
+                            SizedBox(width: 10),
+                            Text("所要時間", style: TextStyle(fontSize: 16)),
+                            Icon(Icons.alarm, color: Colors.orange[300], size: 28),
+                            Spacer(),
+                            Text("01:00", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+
+                        // 時間帯
+                        Row(
+                          children: [
+                            SizedBox(width: 10),
+                            Text("時間帯", style: TextStyle(fontSize: 16)),
+                            Icon(Icons.timelapse_outlined, color: Colors.orange[300], size: 28),
+                            Spacer(),
+                            Text("17:00 ~ 18:00", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+
+                        // 開催ボタン 仮でRR画面に遷移してます
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => RealtimeRoutine(
+                                  routine: routine,
+                                ),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: ColorConst.bt,
+                            shape: StadiumBorder(),
+                            elevation: 5,
+                            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 12),
+                          ),
+                          child: Text("開催", style: TextStyle(color: Colors.white, fontSize: 16)),
+                        ),
+                      ],
+                    ),
+
+                    // ✕ ボタン（右上）
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+      style: ElevatedButton.styleFrom(
+        shape: const CircleBorder(),
+        padding: const EdgeInsets.all(10),
+        backgroundColor: ColorConst.bt,
+        elevation: 4,
+      ),
+      child: const SizedBox(
+        width: 80,
+        height: 80,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                '開催',
+                style: TextStyle(color: ColorConst.swout),
+              ),
+              SizedBox(height: 4),
+              Icon(
+                Icons.local_fire_department_rounded,
+                size: 40,
+                color: ColorConst.swout,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
 }
